ca0ce9dbb8a310bd7ad8cd7f3f490fb5c6d91068
rdia221 <xrida3852@gmail.com>
2026-01-31 18:33:23 +0500

    Game design

--- src/components/ui/GenreList.tsx
+++ src/components/ui/GenreList.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import useGenres, { Genre } from "../../hooks/useGenres";
 import useData from "../../hooks/useData";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../../hooks/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
+  function getCroppedImageURL(image_background: string): string | undefined {
+    throw new Error("Function not implemented.");
+  }
+
   return (
-    <ul>
+    <List padding={1}>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id}>
+          <HStack padding={1}>
+            <Image
+              boxSize="32px "
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
